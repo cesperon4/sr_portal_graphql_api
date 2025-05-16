a2a7366d97aa7b46ad6d55a6c0e27a97053321c3
cesperon <cesperon4@gmail.com>
2025-05-16 08:41:40 -0700

    turn on httpOnly

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -137,7 +137,7 @@ export const userResolvers = {
         //cookie header set
         "Set-Cookie",
         serialize("token", token, {
-          // httpOnly: true,
+          httpOnly: true,
           secure: process.env.NODE_ENV === "production", // true in production
           // sameSite: "lax", // or "Strict" if you prefer tighter CSRF protection
           sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // or "Strict" if you prefer tighter CSRF protection
